752683fba312cef20b66ac9e599ec1cc985f3ea3
mirzahm14 <mirzahm14@gmail.com>
2024-05-27 15:02:00 +0700

    db: modify table n relation

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ const docs = require('../docs/v1.json')
 // Router.use('/tickets', ticketRoutes)
 
 Router.get('/', (req,res) => {
+    console.log(req.body)
     res.status(200).json({
         status: true,
         message: 'Connected to Server!'
