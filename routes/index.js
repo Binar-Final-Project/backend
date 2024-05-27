f2560c0da773e8de3ce60e185314466d2bb8fbee
mirzahm14 <mirzahm14@gmail.com>
2024-05-27 15:03:16 +0700

    remove: delete console

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,6 @@ const docs = require('../docs/v1.json')
 // Router.use('/tickets', ticketRoutes)
 
 Router.get('/', (req,res) => {
-    console.log(req.body)
     res.status(200).json({
         status: true,
         message: 'Connected to Server!'
