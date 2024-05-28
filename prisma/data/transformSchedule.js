c5f411e8ae05009a51bd5ad2ad295a743eb71390
mirzahm14 <mirzahm14@gmail.com>
2024-05-28 18:10:43 +0700

    db: progress schedule seeding

--- prisma/data/transformSchedule.js
+++ prisma/data/transformSchedule.js
@@ -1,4 +1,21 @@
-const schedules = require('./schedules.json')
-for(let i = 0; i<5;i++){
-    console.log(schedules[i])
+// const scheduleRaw = require('./schedules.json')
+// const dict = require('./airportDictionary.json')
+// const transformedSchedule = []
+
+// for(let i = 0; i<10; i++){
+//     const sch = {
+//         flight_number: scheduleRaw[i].flight_number,
+//         departure_airport_id: dict[scheduleRaw[i].departure_airport],
+//         arrival_airport_id: dict[scheduleRaw[i].arrival_airport]
+//     }
+// }
+
+
+const fs = require('fs')
+const sch = require('./transformedSchedule.json')
+
+const main = async function(){
+    
 }
+
+main()
