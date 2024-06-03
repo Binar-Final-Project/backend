794ed63db623503742aea64f13c1cc5730c11cb7
putrinisarengganis <putrinisarengganis@gmail.com>
2024-06-03 22:48:38 +0700

    feat: history

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,8 @@ const authRoutes = require('./auth.routes')
 Router.use('/users', authRoutes)
 const flightRouter = require('./flight.routes')
 Router.use('/flights', flightRouter)
+const transactionsRouter = require('./transactions.routes')
+Router.use('/transactions', transactionsRouter)
 
 Router.get('/', (req,res) => {
     res.status(200).json({
