9a74dc56ff8d08fafc112c032a6c3fba4b0fefd3
Mirza <mirzahm14@gmail.com>
2024-06-21 19:32:25 +0700

    feat: add transaction time

--- controllers/transaction.controller.js
+++ controllers/transaction.controller.js
@@ -94,6 +94,15 @@ const history = async (req, res, next) => {
       const tr = {};
       tr.flights = [t.ticket.departure_flight.flight_id];
       tr.transaction_id = t.transaction_id;
+      tr.transaction_date = new Date(t.created_at).toLocaleString("en-US", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        timeZone: "Asia/Jakarta",
+      })
       tr.total_price = t.total_price;
       tr.total_before_tax = +t.total_price - +t.tax;
       tr.tax = t.tax;
